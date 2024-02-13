853d29f6029b7a3932c39576258def86f0fe1dbc
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-02-13 06:50:08 +0000

    I generated auto config files for firebase

--- lib/backend/firebase/firebase_config.dart
+++ lib/backend/firebase/firebase_config.dart
@@ -1,5 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 
 Future initFirebase() async {
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyBBJeHbAoLlimbH4cdAYHRu-rhFzH2MJP0",
+            authDomain: "unicoduex-511fy9.firebaseapp.com",
+            projectId: "unicoduex-511fy9",
+            storageBucket: "unicoduex-511fy9.appspot.com",
+            messagingSenderId: "941789552403",
+            appId: "1:941789552403:web:bac18d5eb4d81d969804e6"));
+  } else {
+    await Firebase.initializeApp();
+  }
 }
