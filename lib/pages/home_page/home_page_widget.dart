1871171e40e6b866b9d65620196b1d0e88813bff
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-02-13 06:29:16 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/home_page/home_page_widget.dart
+++ lib/pages/home_page/home_page_widget.dart
@@ -52,7 +52,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
           backgroundColor: FlutterFlowTheme.of(context).primary,
           automaticallyImplyLeading: false,
           title: Text(
-            'hi',
+            'this is the future',
             style: FlutterFlowTheme.of(context).headlineMedium.override(
                   fontFamily: 'Outfit',
                   color: Colors.white,
